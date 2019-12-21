3b8a4dedc01357987070b38d119adb898b481fd8
Abishek <Abishek@192.168.1.22>
2019-12-21 09:53:13 +0530

    First Commit [21/12/2019]

---
+++ src/test/java/TM_Selenium_HRM/TM_Selenium_HRM/HRM_Login.java
@@ -0,0 +1,15 @@
+package TM_Selenium_HRM.TM_Selenium_HRM;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class HRM_Login {
+
+	public static void main(String[] args) 
+	{
+     System.setProperty("webdriver.gecko.driver","C:\\Users\\Abishek\\Desktop\\Selenium\\geckodriver.exe");
+     WebDriver driver = new FirefoxDriver();
+     driver.get("http://testingmasters.com/hrm/symfony/web/index.php/auth/login");
+    }
+
+}
