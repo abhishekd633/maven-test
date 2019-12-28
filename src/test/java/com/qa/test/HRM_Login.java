a155a19992cb81d395b52f7ff6138f5998ba3bb2
nagaphanidoppalapudi <59003667+nagaphanidoppalapudi@users.noreply.github.com>
2019-12-28 09:58:22 +0530

    First Commit[28/12/2019]

--- src/test/java/com/qa/test/HRM_Login.java
+++ src/test/java/com/qa/test/HRM_Login.java
@@ -1,16 +1,21 @@
-package TM_Selenium_HRM.TM_Selenium_HRM;
+package com.qa.test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
 public class HRM_Login {
 
-	public static void main(String[] args) 
+	@Test
+	
+	public void login()
 	{
      System.setProperty("webdriver.gecko.driver","C:\\Users\\Abishek\\Desktop\\Selenium\\geckodriver.exe");
      WebDriver driver = new FirefoxDriver();
      driver.get("http://testingmasters.com/hrm/symfony/web/index.php/auth/login");
      //comment Text Text
+     
+     //twsr
      
      
     }
